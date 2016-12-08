8f9176cf1b70b80bd650bd3483970f7a9b6b6b14
Friederike Duendar <frd2007@med.cornell.edu>
2016-12-08 14:02:48 -0500

    initial commit for R package

---
+++ R/functions.R
@@ -0,0 +1,8 @@
+#' @title Christmas Date
+#' @description Print the day Christmas falls on.
+#' @export
+print_date <- function(){
+  day <- "Sunday"
+  date <- "25 Dec 2016"
+  print(paste(day, date))
+}
